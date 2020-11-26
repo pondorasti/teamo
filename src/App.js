0a50a53d82adb19ed24a8e28654f9879e6333299
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-11-25 22:58:29 -0800

    Refactored custom theme into TMTheme

--- src/App.js
+++ src/App.js
@@ -1,55 +1,24 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/core/styles'
+import TMTheme from './atoms/TMTheme'
+
 import { makeStyles } from '@material-ui/core/styles'
-import { Paper, Divider } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#7800D7',
-      dark: '#7800D7'
-    },
-    secondary: {
-      main: '#AAA3EB',
-      dark: '#AAA3EB'
-    },
-    warning: {
-      main: '#4D287C',
-      dark: '#4D287C'
-    },
-    error: {
-      main: '#FF453A99',
-      dark: '#FF453A99'
-    },
-    info: {
-      main: '#0A84FF',
-      dark: '#0A84FF'
-    },
-
-    text: {
-      primary: "#FFFFFFE6",
-      secondary: "#FFFFFF66",
-      disabled: "#FFFFFF40",
-      hint: "#FFFFFF40",
-    },
-
-    divider: '#FFFFFF26',
-
-    tonalOffset: 0,
-    contrastThreshold: 0,
-
-    type: 'dark'
-  },
-  shadows: ['none']
+const useStyles = makeStyles({
+  root: {
+    padding: '4px 8px',
+    borderRadius: 8
+  }
 })
 
 function App() {
   const classes = useStyles()
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={TMTheme}>
       <Paper>
 
         <Button className={classes.root} variant="contained" color="primary">Primary</Button> <br></br>
