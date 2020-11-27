6d6fb6a799be45dfe87490865293980de3b806ea
Tian Qiu <tian.qiu142@gmail.com>
2020-11-26 18:27:59 -0800

    almost done with avatar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
-import { ThemeProvider } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/core/styles';
 
-import TMTheme from './atoms/TMTheme'
-import TMButton from './atoms/TMButton'
+import TMTheme from './atoms/TMTheme';
+// import TMButton from './atoms/TMButton';
+import TMAvatar from './atoms/TMAvatar';
 
-import LanguageIcon from './assets/icons/Language.js';
+// import LanguageIcon from './assets/icons/Language.js';
 
 import { Paper, SvgIcon, Button } from '@material-ui/core';
 
 function App() {
   return (
     <ThemeProvider theme={TMTheme}>
-      <Paper>
-
-        <TMButton size="small" disabled>Hey there</TMButton>
-      </Paper>
+      {/* <Paper>
+        <TMButton size="small" disabled>
+          Hey there
+        </TMButton>
+      </Paper> */}
+      <TMAvatar src="https://qph.fs.quoracdn.net/main-qimg-3d69658bf00b1e706b75162a50d19d6c" />
     </ThemeProvider>
   );
 }
