66fcb92bcec85f6aa8d99bb290a983fcb7313fde
tian142 <tian.qiu142@gmail.com>
2020-11-28 00:12:14 -0800

    Started on Room Cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from 'react'
 
 import { ThemeProvider } from '@material-ui/core/styles'
 
 import TMTheme from './atoms/TMTheme'
 import TMButton from './atoms/TMButton'
+import TMCard from './atoms/TMCard'
+import LanguageIcon from './assets/icons/Language.js'
 
-import LanguageIcon from './assets/icons/Language.js';
-
-import { Paper, SvgIcon, Button } from '@material-ui/core';
+import { Paper, SvgIcon, Button } from '@material-ui/core'
 
 function App() {
   return (
     <ThemeProvider theme={TMTheme}>
       <Paper>
-
-        <TMButton size="small" disabled>Hey there</TMButton>
+        <TMCard />
       </Paper>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
