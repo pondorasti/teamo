c403db9a42e4fa4aeef01d77a34975c7d06d383d
tian142 <tian.qiu142@gmail.com>
2020-11-29 14:26:05 -0800

    Almost done with selected game display

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles'
 
-import { Paper, CssBaseline } from '@material-ui/core';
-import TMTheme from './atoms/TMTheme';
-import TMTextfield from './atoms/TMTextfield';
-import TMAutocomplete from './atoms/TMAutocomplete';
+import { Paper, CssBaseline } from '@material-ui/core'
+import TMTheme from './atoms/TMTheme'
+import TMTextfield from './atoms/TMTextfield'
+import TMAutocomplete from './atoms/TMAutocomplete'
 import AppBar from './screens/TMAppBar'
+import TMSelectedGameInfo from './screens/TMSelectedGameInfo'
 
 function App() {
-
   const games = ['Minecraft', 'League of Legends', 'Among Us']
 
   return (
@@ -17,14 +17,26 @@ function App() {
       <AppBar />
       <CssBaseline />
       <Paper style={{ padding: 40 }}>
-        <TMTextfield label="Games" defaultValue="jello" helperText="hello" rows={4} multiline />
-        <TMTextfield label="Games" defaultValue="jello" type="number"/>
-        <TMAutocomplete options={games} getOptionLabel={(game) => game}></TMAutocomplete>
+        <TMTextfield
+          label="Games"
+          defaultValue="jello"
+          helperText="hello"
+          rows={4}
+          multiline
+        />
+        <TMTextfield label="Games" defaultValue="jello" type="number" />
+        <TMAutocomplete
+          options={games}
+          getOptionLabel={(game) => game}
+        ></TMAutocomplete>
+        <TMSelectedGameInfo
+          selectGameImg="https://cdn.vox-cdn.com/thumbor/2D0fSxmi24Zw7aaB3M_TViUavrc=/1400x788/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/15957232/0fe20042_0bb8_4781_82f4_7130f928b021.jpg"
+          selectGameName="Minecraft"
+          selectGameStats="150 Players,  43 Lobbies"
+        />
       </Paper>
     </ThemeProvider>
   )
 }
-
-
 
 export default App
---
+++ src/screens/TMSelectedGameInfo.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import { Typography } from '@material-ui/core'
+import TMAutocomplete from '../atoms/TMAutocomplete'
+
+const games = ['Minecraft', 'League of Legends', 'Among Us']
+const platforms = ['PC', 'Play Station', 'XBOX', 'VR']
+
+const useStyles = makeStyles((theme) => ({
+  selectGameDiv: {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '20px 0px',
+    padding: '20px 0px',
+    backgroundColor: theme.palette.grey[800],
+  },
+  leftSideDiv: {
+    display: 'flex',
+  },
+
+  gameImg: {
+    width: 152,
+    height: 72,
+    objectFit: 'cover',
+  },
+  gameInfo: {
+    flexDirection: 'column',
+    marginLeft: 10,
+  },
+  gameStats: {
+    marginTop: 4,
+  },
+  divSpacer: {
+    flexGrow: 1,
+  },
+  selections: {
+    display: 'flex',
+    alignItems: 'space-between',
+  },
+}))
+
+function TMSelectedGameInfo({
+  selectGameImg,
+  selectGameName,
+  selectGameStats,
+}) {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.selectGameDiv}>
+      <div className={classes.leftSideDiv}>
+        <img
+          src={selectGameImg}
+          alt="selected game"
+          className={classes.gameImg}
+        />
+
+        <div className={classes.gameInfo}>
+          <Typography variant="h4">{selectGameName}</Typography>
+          <Typography variant="body1" classes={{ root: classes.gameStats }}>
+            {selectGameStats}
+          </Typography>
+        </div>
+      </div>
+
+      <div className={classes.divSpacer} />
+
+      <div className={classes.selections}>
+        <TMAutocomplete
+          label="Game"
+          options={games}
+          getOptionLabel={(game) => game}
+        ></TMAutocomplete>
+        <TMAutocomplete
+          label="Platform"
+          options={platforms}
+          getOptionLabel={(platform) => platform}
+        ></TMAutocomplete>
+      </div>
+    </div>
+  )
+}
+
+export default TMSelectedGameInfo
