3c99f5a387f5ba9be068d99cee1444d2ce6a6b85
tian142 <tian.qiu142@gmail.com>
2020-11-28 17:56:22 -0800

    Changed className to classes, + props to card/card footer, + styling

--- src/atoms/TMCard.js
+++ src/atoms/TMCard.js
@@ -3,77 +3,86 @@ import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
 import { makeStyles } from '@material-ui/core/styles'
 import { Grid } from '@material-ui/core'
+import PropTypes from 'prop-types'
 
-import TMCardStats from './TMCardStats'
+import TMCardFooter from './TMCardFooter'
 
 const useStyles = makeStyles((theme) => ({
   lobbyCard: {
-    width: 290,
-    // minWidth: 286,
-    // height: 150,
-    padding: '15px 15px',
-    cursor: 'pointer',
-    backgroundColor: theme.palette.grey[700],
+    width: 284,
+    // minWidth: 284,
+    padding: 12,
     borderRadius: 16,
     transition: '70ms',
+    cursor: 'pointer',
+    backgroundColor: theme.palette.grey[700],
     // '&:hover': {
     //   transform: 'translate(-3px, -3px)',
     //   boxShadow: '0px 8px 20px 1px rgba(56, 56, 56, 0.2)',
     // },
   },
-  gameImg: {
+  topDiv: {
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 14,
+  },
+  gameImgGrid: {
     display: 'flex',
     justifyContent: 'flex-end',
+  },
+  gameImg: {
     width: 84,
     height: 24,
     objectFit: 'cover',
   },
-  cardTopSection: {
+  cardMiddleSection: {
     marginBottom: 20,
   },
-  cardMiddleSection: {
-    marginBottom: 22,
-  },
-  cardBtoomSection: {
-    marginBottom: 0,
-  },
-
   userNameTypography: {
-    paddingTop: 4,
     color: theme.palette.text.secondary,
-  }
+  },
 }))
-
-function TMCard() {
+// https://logos-world.net/wp-content/uploads/2020/04/Minecraft-Logo.png
+function TMCard({ userName, gameLogo, decsription }) {
   const classes = useStyles()
 
   return (
-    <Card className={classes.lobbyCard} variant="outlined">
+    <Card classes={{ root: classes.lobbyCard }} variant="outlined">
       <Grid container justify="space-between">
-        <Grid item xs={6} className={classes.cardTopSection}>
-          <Typography variant='body1' classes={{ root: classes.userNameTypography }}>
-            Pic UserName123
-          </Typography>
-        </Grid>
-        <Grid item xs={6} className={classes.gameImg}>
-          <img
-            className={classes.gameImg}
-            src="https://logos-world.net/wp-content/uploads/2020/04/Minecraft-Logo.png"
-            alt="gameImg"
-          />
-        </Grid>
-        <Grid item xs={12} className={classes.cardMiddleSection}>
-          <Typography variant='h5'>
-            This is my room decription, this should be no more than three lines
-            long...
-          </Typography>
+        <div className={classes.topDiv}>
+          <Grid item xs={6}>
+            <Typography
+              variant="body1"
+              classes={{ root: classes.userNameTypography }}
+            >
+              {userName}
+            </Typography>
+          </Grid>
+          <Grid item xs={6} classes={{ root: classes.gameImgGrid }}>
+            <img className={classes.gameImg} src={gameLogo} alt="gameImg" />
+          </Grid>
+        </div>
+        <Grid item xs={12} classes={{ root: classes.cardMiddleSection }}>
+          <Typography variant="h5">{decsription}</Typography>
         </Grid>
         <Grid item xs={12}>
-          <TMCardStats className={classes.cardBtoomSection} />
+          <TMCardFooter gamePlatform="PC" micChoice="Mic" roomSize="3/5" />
         </Grid>
       </Grid>
     </Card>
   )
+}
+
+TMCard.propTypes = {
+  /** The room master's name  */
+  userName: PropTypes.string,
+
+  /** The small game logo  */
+  gameLogo: PropTypes.string,
+
+  /** The description of room*/
+  decsription: PropTypes.string,
 }
 
 export default TMCard
