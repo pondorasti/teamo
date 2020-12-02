ae652446e8479d0c541a3660be99bbb0c73dbd1f
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-12-02 08:27:27 -0800

    Finished LobbyCard

--- src/screens/home-screen/molecules/LobbyCard.js
+++ src/screens/home-screen/molecules/LobbyCard.js
@@ -1,29 +1,30 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
-import { Grid, IconButton, Menu, MenuItem, Card, CardActionArea, Typography } from "@material-ui/core"
+import { Grid, Card, CardActionArea, Typography } from "@material-ui/core"
 import PropTypes from "prop-types"
 
 import LobbyCardFooter from "./LobbyCardFooter"
+import LobbyOptionsButton from "./LobbyOptionsButton"
 import TMAvatar from "../../../atoms/TMAvatar"
-import ThreeDots from "../../../assets/icons/ThreeDots"
 
 const useStyles = makeStyles((theme) => ({
   cardContainer: { position: "relative" },
   card: {
     borderRadius: 16,
     border: `1px solid ${theme.palette.grey[500]}`,
-    WebkitMaskImage: "-webkit-radial-gradient(white, black)", // fixes safari overflow bug
+    
+    // fixes safari overflow bug
+    WebkitMaskImage: "-webkit-radial-gradient(white, black)", 
 
-    pointerEvents: "fill", // don't accidently trigger options menu by hovering over the border
+    // don't accidently trigger options menu by hovering over the border
+    pointerEvents: "fill", 
     backgroundColor: theme.palette.grey[700],
     "&:hover + $optionsButtonContainer": {
       opacity: 1,
       pointerEvents: "auto",
     },
   },
-  cardActionArea: {
-    padding: 12,
-  },
+  cardActionArea: { padding: 12 },
   cardActionAreaFocusHighlight: {},
   topDiv: {
     display: "flex",
@@ -51,64 +52,16 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
     marginLeft: 4,
   },
-  optionsButtonContainer: {
-    border: `1px solid ${theme.palette.grey[500]}`,
-    backgroundColor: theme.palette.grey[700],
-    borderRadius: 6,
-
-    overflow: "hidden",
-    position: "absolute",
-    zIndex: 1,
-
-    top: -12,
-    right: -12,
-
-    opacity: 0,
-
-    // do not reveal options menu before hovering the card
-    pointerEvents: "none", 
-
-    transition: theme.transitions.create("opacity",  {
-      duration: theme.transitions.duration.short,
-    }),
-
-    "&:hover": {
-      pointerEvents: "auto",
-      opacity: 1,
-    },
-  },
-  optionsIcon: {
-    borderRadius: 0,
-    height: 24,
-    width: 24,
-  },
-  optionIconStyle: { fontSize: 16 },
+  optionsButtonContainer: {}, // decoy class used for referencing
 }))
 
 function LobbyCard({ hostUsername, hostPicture, gameName, gameLogo, description, platform, usesMic, sizeStatus }) {
   const classes = useStyles()
-  const [anchorEl, setAnchorEl] = React.useState(null)
-  const open = Boolean(anchorEl)
-
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
-  const handleOptionsButton = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleAddToWatchList = () => {
-    handleClose()
-  }
-  const handleReportLobby = () => {
-    handleClose()
-  }
 
   return (
     <div className={classes.cardContainer}>
       <Card classes={{ root: classes.card }}>
-        <CardActionArea className={classes.cardActionArea}>
+        <CardActionArea classes={{ root: classes.cardActionArea }}>
           <Grid container justify="space-between">
             <div className={classes.topDiv}>
               <Grid item xs={6} classes={{ root: classes.hostInfo }}>
@@ -138,28 +91,7 @@ function LobbyCard({ hostUsername, hostPicture, gameName, gameLogo, description,
         </CardActionArea>
       </Card>
 
-      <div className={classes.optionsButtonContainer}>
-        <IconButton
-          classes={{ root: classes.optionsIcon }}
-          aria-label="lobby options"
-          aria-controls="lobby-menu"
-          aria-haspopup="true"
-          onClick={handleOptionsButton}
-        >
-          <ThreeDots className={classes.optionIconStyle} />
-        </IconButton>
-        <Menu
-          anchorEl={anchorEl}
-          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-          transformOrigin={{ vertical: "top", horizontal: "right" }}
-          keepMounted
-          open={open}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleAddToWatchList}>Add to Watch List</MenuItem>
-          <MenuItem onClick={handleReportLobby}>Report Lobby</MenuItem>
-        </Menu>
-      </div>
+      <LobbyOptionsButton className={classes.optionsButtonContainer} />
     </div>
   )
 }
