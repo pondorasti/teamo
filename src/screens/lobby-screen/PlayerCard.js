775ad2ad51b869c4e22510abbf4d73a0126e8f61
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-12-09 08:50:04 -0800

    WIP: Player Card
    - Added Options Dropdown Menu
    - Added Collapse animation

--- src/screens/lobby-screen/PlayerCard.js
+++ src/screens/lobby-screen/PlayerCard.js
@@ -1,16 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { makeStyles } from "@material-ui/core/styles"
-import { Grid, Typography, Tooltip } from "@material-ui/core"
-import { Copy } from "../../assets/icons"
+import { Grid, Typography, Tooltip, IconButton, Menu, MenuItem, Collapse } from "@material-ui/core"
+import { useTheme } from "@material-ui/styles"
+import { Copy, ThreeDots, Exit, Headset, Crown } from "../../assets/icons"
 import { TMAvatar, TMButton } from "../../atoms/"
 
 const useStyles = makeStyles(theme => ({
   containerGrid: {
     width: 304,
     borderRadius: 16,
-    backgroundColor: "#4D287C",
     padding: 16,
+    transition: theme.transitions.create("background-color"),
   },
   headerGrid: {
     marginBottom: 8,
@@ -34,13 +35,27 @@ const useStyles = makeStyles(theme => ({
 
 function PlayerCard({ username }) {
   const classes = useStyles()
+  const theme = useTheme()
+
+  const [acceptedPlayer, setAcceptedPlayer] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+
+  function handleClose() { setAnchorEl(null) }
+  function handleOptionsButton(event) { setAnchorEl(event.currentTarget) }
+  function handledAcceptButton() { setAcceptedPlayer(true) }
 
   function copyGamerTagToClipboard() {
     navigator.clipboard.writeText(username)
   }
 
   return (
-    <Grid container direction="column" classes={{ root: classes.containerGrid }}>
+    <Grid
+      container
+      direction="column"
+      classes={{ root: classes.containerGrid }}
+      style={{ backgroundColor: `${acceptedPlayer ? theme.palette.warning.main : theme.palette.grey[700]}` }}
+    >
       <Grid container item wrap="nowrap" classes={{ root: classes.headerGrid }}>
         <TMAvatar
           size="medium"
@@ -51,11 +66,12 @@ function PlayerCard({ username }) {
         <Grid container item direction="column" style={{ marginLeft: 8 }} >
           <Typography variant="h5">
             Pondorasti
+            <Crown />
           </Typography>
 
           <Tooltip title="Copy" arrow placement="right">
             <div onClick={copyGamerTagToClipboard} className={classes.containerGamerTag}>
-              <Typography variant="body">
+              <Typography variant="body1">
                 u/{username}
               </Typography>
               <Copy style={{ marginLeft: 4, width: 12, height: 12 }} />
@@ -63,27 +79,47 @@ function PlayerCard({ username }) {
           </Tooltip>
         </Grid>
 
+        <IconButton style={{ width: 24, height: 24 }} onClick={handleOptionsButton}>
+          <ThreeDots size="inherit" />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          transformOrigin={{ vertical: "top", horizontal: "left" }}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem>Profile</MenuItem>
+          <MenuItem>Kick</MenuItem>
+        </Menu>
+
       </Grid>
 
       <Typography variant="body1">
         we only allow this many characters? 52 chars, let&apos;s make it 62
       </Typography>
 
-      <Grid container item wrap="nowrap" classes={{ root: classes.buttonsGrid }}>
-        <TMButton
-          variant="outlined"
-          fullWidth
-        >
-          Deny
-        </TMButton>
-        <TMButton
-          fullWidth
-          color="primary"
-          style={{ marginLeft: 16 }}
-        >
-          Accept
-        </TMButton>
-      </Grid>
+      <Collapse in={!acceptedPlayer}>
+        <Grid container item wrap="nowrap" classes={{ root: classes.buttonsGrid }}>
+          <TMButton
+            fullWidth
+            variant="outlined"
+            leadingIcon={<Exit />}
+          >
+            Deny
+          </TMButton>
+
+          <TMButton
+            fullWidth
+            color="primary"
+            leadingIcon={<Headset />}
+            style={{ marginLeft: 16 }}
+            onClick={handledAcceptButton}
+          >
+            Accept
+          </TMButton>
+        </Grid>
+      </Collapse>
     </Grid>
   )
 }
