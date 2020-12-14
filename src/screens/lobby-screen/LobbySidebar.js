db400a4644b488a5fe40934566fd1f0e844548e3
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-12-13 17:23:18 -0800

    Finished LobbySidebar
    - Added dummyData
    - Fixed scrollbar issues

--- src/screens/lobby-screen/LobbySidebar.js
+++ src/screens/lobby-screen/LobbySidebar.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles"
 import { Drawer, Toolbar, Grid, Divider } from "@material-ui/core/"
 import LobbyHeader from "./LobbyHeader"
 import PlayerCard from "./PlayerCard"
+import data from "../../lobbyPlayersDummyData"
 
 const drawerWidth = 352
 const useStyle = makeStyles(theme => ({
@@ -12,11 +13,12 @@ const useStyle = makeStyles(theme => ({
   },
   drawerPaper: {
     width: drawerWidth,
-    padding: 24,
+    
     backgroundColor: theme.palette.grey[800]
   },
   drawerContainer: {
     overflow: "auto",
+    padding: 24,
   },
 }))
 
@@ -32,38 +34,37 @@ function LobbySidebar() {
       }}
     >
       <Toolbar />
-      <Grid container spacing={3}>
-        <LobbyHeader
-          lobbyHost="Pondorasti"
-          gameName="Minecraft"
-          gameLogo="https://logos-world.net/wp-content/uploads/2020/04/Minecraft-Logo.png"
-          lobbyDesc="This is my room decription, this should be no more than three lines long..."
-          platform="PC"
-          mic="Microphone"
-          players="4/4 Players, 1 waiting"
-        />
-
-        <Grid item style={{ width: "100%" }}>
-          <Divider style={{ height: "1px" }}/>
-        </Grid>
-
-        <Grid container item spacing={2}>
-          <PlayerCard
-            username="Pondorasti"
-            gamerTag="Lascorpy"
-            avatarUrl="https://avatars0.githubusercontent.com/u/32957606?s=460&u=e631c3762c12d41f3ce0348b8137f0751a2eed75&v=4"
-            bio="we only allow this many characters? 52 chars, let's make it 62"
+      <div className={classes.drawerContainer}>
+        <Grid container spacing={3} >
+          <LobbyHeader
+            lobbyHost="Pondorasti"
+            gameName="Minecraft"
+            gameLogo="https://logos-world.net/wp-content/uploads/2020/04/Minecraft-Logo.png"
+            lobbyDesc="This is my room decription, this should be no more than three lines long..."
+            platform="PC"
+            mic="Microphone"
+            players="4/4 Players, 1 waiting"
           />
-          <PlayerCard
-            username="Pondorasti"
-            gamerTag="Lascorpy"
-            avatarUrl="https://avatars0.githubusercontent.com/u/32957606?s=460&u=e631c3762c12d41f3ce0348b8137f0751a2eed75&v=4"
-            bio="we only allow this many characters? 52 chars, let's make it 62"
-            isHost
-            isAccepted
-          />
+
+          <Grid item style={{ width: "100%" }}>
+            <Divider style={{ height: "1px" }} />
+          </Grid>
+
+          <Grid container item spacing={2}>
+            {data.map((player, index) => (
+              <PlayerCard
+                key={index}
+                username={player.username}
+                gamerTag={player.gamerTag}
+                avatarUrl={player.avatarUrl}
+                bio={player.bio}
+                isHost={player.isHost}
+                isAccepted={player.isAccepted}
+              />
+            ))}
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </Drawer>
   )
 }
