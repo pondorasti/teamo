b5d48b911d395b500c92ba84718645c4eb8091ae
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-12-14 23:24:12 -0800

    Fixed LobbyOptionButton Menu error

--- src/screens/home-screen/molecules/LobbyOptionsButton.js
+++ src/screens/home-screen/molecules/LobbyOptionsButton.js
@@ -76,7 +76,7 @@ function LobbyOptionsButton({className}) {
       </IconButton>
       <Menu
         anchorEl={anchorEl}
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
         transformOrigin={{ vertical: "top", horizontal: "right" }}
         open={open}
         onClose={handleClose}
