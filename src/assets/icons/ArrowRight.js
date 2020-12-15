8d99af258d13c87670e12f75eca4ca35d813a4a7
tian142 <tian.qiu142@gmail.com>
2020-12-15 12:46:37 -0800

    Added Arrow Left,Right Icons

---
+++ src/assets/icons/ArrowRight.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { SvgIcon } from "@material-ui/core"
+
+function ArrowRightIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M9.29006 8.12002L13.1701 12L9.29006 15.88C8.90006 16.27 8.90006 16.9 9.29006 17.29C9.68006 17.68 10.3101 17.68 10.7001 17.29L15.2901 12.7C15.6801 12.31 15.6801 11.68 15.2901 11.29L10.7001 6.70002C10.3101 6.31002 9.68006 6.31002 9.29006 6.70002C8.91006 7.09002 8.90006 7.73002 9.29006 8.12002V8.12002Z" />
+    </SvgIcon>
+  )
+}
+
+export default ArrowRightIcon
