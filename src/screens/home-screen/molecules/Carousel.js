556e70da64689c5513f141b5e86108a391bcb932
tian142 <tian.qiu142@gmail.com>
2020-12-15 17:59:32 -0800

    Moved styling from css file to component

--- src/screens/home-screen/molecules/Carousel.js
+++ src/screens/home-screen/molecules/Carousel.js
@@ -5,40 +5,56 @@ import NextArrow from "./NextArrow"
 import PrevArrow from "./PrevArrow"
 import "./Carousel.css"
 import LobbyCard from "./LobbyCard"
+import classNames from "classnames"
 
 const useStyles = makeStyles({
   slider: {
-    width: "50%",
+    width: 800,
+    paddingTop: 16,
     display: "flex",
     alignItems: "center",
   },
-  card: {
-    // width: 600,
+  cards: {
+    // width: 400,
     // height: 200,
+    margin: 32,
+    // backgroundColor: "red",
+  },
+  slide: {
+    transform: "scale(0.7)",
+    transition: "transform 300ms",
+    opacity: 0.5,
+    margin: 16,
+  },
+
+  activeSlide: {
+    transform: "scale(1.1)",
+    opacity: 1,
+    marginLeft: "-50px",
+    marginRight: "-50px",
+    marginTop: 8,
+    padding: 16,
+    zIndex: 9,
+    position: "relative",
+    filter: "drop-shadow(0px 0px 100px rgba(0, 0, 0, 0.75))",
   },
 })
 
 function Carousel() {
+  const classes = useStyles()
   const [imageIndex, setImageIndex] = useState(0)
 
-  const classes = useStyles()
+  const mapCarousel = lobbies.map((lobby, index) => {
+    const className = classNames(
+      {
+        [classes.activeSlide]: index === imageIndex,
+      },
+      classes.slide
+    )
 
-  const settings = {
-    infinite: true,
-    lazyLoad: true,
-    speed: 300,
-    slidesToShow: 3,
-    centerMode: true,
-    centerPadding: 0,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    beforeChange: (current, next) => setImageIndex(next),
-  }
-
-  return (
-    <Slider {...settings} className={classes.slider}>
-      {lobbies.map((lobby, index) => (
-        <div key={index} className={index === imageIndex ? "slide activeSlide" : "slide"}>
+    return (
+      <div className={classes.cards} key={index}>
+        <div className={className}>
           <LobbyCard
             hostUsername={lobby.username}
             hostPicture="https://qph.fs.quoracdn.net/main-qimg-3d69658bf00b1e706b75162a50d19d6c"
@@ -48,10 +64,32 @@ function Carousel() {
             platform="PS5"
             usesMic={false}
             sizeStatus="3/5"
-            className={classes.card}
           />
         </div>
-      ))}
+      </div>
+    )
+  })
+
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    focusOnSelect: true,
+    // autoplay: true,
+    // autoplaySpeed: 6000,
+    speed: 400,
+    pauseOnHover: true,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 30,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next),
+    className: classes.slide,
+  }
+
+  return (
+    <Slider {...settings} className={classes.slider}>
+      {mapCarousel}
     </Slider>
   )
 }
@@ -60,22 +98,22 @@ export default Carousel
 
 const lobbies = [
   {
-    username: "Pondorasti",
+    username: "Card - 1",
     gameLogo: "https://logos-world.net/wp-content/uploads/2020/04/Minecraft-Logo.png",
     description: "This is my room decription, this should be no more than three lines long...",
   },
   {
-    username: "Pondorasti",
+    username: "Card - 2",
     gameLogo: "https://logos-world.net/wp-content/uploads/2020/04/Minecraft-Logo.png",
     description: "This is my room decription, this should be no more than three lines long...",
   },
   {
-    username: "Pondorasti",
+    username: "Card - 3",
     gameLogo: "https://logos-world.net/wp-content/uploads/2020/04/Minecraft-Logo.png",
     description: "This is my room decription, this should be no more than three lines long...",
   },
   {
-    username: "Pondorasti",
+    username: "Card - 4",
     gameLogo: "https://logos-world.net/wp-content/uploads/2020/04/Minecraft-Logo.png",
     description: "This is my room decription, this should be no more than three lines long...",
   },
