351b2e1c610b008adea4a1e6e118a1ade3e01f18
tian142 <tian.qiu142@gmail.com>
2020-12-15 23:10:43 -0800

    Imported Hero Card into Hero Carousel

--- src/screens/home-screen/molecules/Carousel.js
+++ src/screens/home-screen/molecules/Carousel.js
@@ -1,47 +1,64 @@
 import React, { useState } from "react"
 import Slider from "react-slick"
 import { makeStyles } from "@material-ui/core/styles"
-import NextArrow from "./NextArrow"
-import PrevArrow from "./PrevArrow"
+import { ArrowLeft, ArrowRight } from "../../../assets/icons"
 import "./Carousel.css"
-import LobbyCard from "./LobbyCard"
 import classNames from "classnames"
+import HeroCard from "./HeroCard"
+import { IconButton } from "@material-ui/core"
+import PropTypes from "prop-types"
 
 const useStyles = makeStyles({
   container: {
     width: "100%",
   },
   slider: {
-    width: "60%",
-    paddingTop: 16,
+    width: "100%",
     display: "flex",
     alignItems: "center",
-  },
-  cards: {
-    // width: 400,
-    // height: 200,
-    // margin: 32,
-    // backgroundColor: "red",
+
+    "& > .slick-list > .slick-track": {
+      paddingTop: 24,
+      paddingBottom: 24,
+    },
   },
   slide: {
+    width: 400,
     transform: "scale(0.7)",
     transition: "transform 300ms",
     opacity: 0.5,
-    margin: 8,
   },
 
   activeSlide: {
     transform: "scale(1.1)",
     opacity: 1,
-    marginLeft: "-50px",
-    marginRight: "-50px",
-    // marginTop: 8,
-    padding: 16,
     zIndex: 9,
     position: "relative",
     filter: "drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.75))",
   },
 })
+
+const PrevArrow = ({ onClick }) => {
+  return (
+    <IconButton size="small" className="arrow next" onClick={onClick}>
+      <ArrowLeft style={{ fontSize: 48 }} />
+    </IconButton>
+  )
+}
+
+const NextArrow = ({ onClick }) => {
+  return (
+    <IconButton size="small" className="arrow next" onClick={onClick}>
+      <ArrowRight style={{ fontSize: 48 }} />
+    </IconButton>
+  )
+}
+PrevArrow.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}
+NextArrow.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}
 
 function Carousel() {
   const classes = useStyles()
@@ -56,17 +73,18 @@ function Carousel() {
     )
 
     return (
-      <div className={classes.cards} key={index}>
+      <div key={index}>
         <div className={className}>
-          <LobbyCard
+          <HeroCard
             hostUsername={lobby.username}
             hostPicture="https://qph.fs.quoracdn.net/main-qimg-3d69658bf00b1e706b75162a50d19d6c"
-            gameLogo={lobby.gameLogo}
+            gameImg="https://cdn.mos.cms.futurecdn.net/MbZ8Yv6WNxjJkPaoQDUPLG-1200-80.jpg"
             gameName="Minecraft"
             description={lobby.description}
             platform="PS5"
-            usesMic={false}
+            usesMic={true}
             sizeStatus="3/5"
+            isContentHidden={index === imageIndex}
           />
         </div>
       </div>
@@ -79,12 +97,12 @@ function Carousel() {
     focusOnSelect: true,
     // autoplay: true,
     // autoplaySpeed: 6000,
-    speed: 400,
+    speed: 300,
     pauseOnHover: true,
     slidesToShow: 3,
-    // slidesToScroll: 3,
+    slidesToScroll: 3,
     centerMode: true,
-    // centerPadding: 30,
+    // centerPadding: 60,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
     beforeChange: (current, next) => setImageIndex(next),
