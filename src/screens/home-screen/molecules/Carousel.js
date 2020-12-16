a862bfabfdaf875142ec3d989550ebd7b9f89ba9
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-12-16 13:47:44 -0800

    WIP: Carousel

--- src/screens/home-screen/molecules/Carousel.js
+++ src/screens/home-screen/molecules/Carousel.js
@@ -9,53 +9,48 @@ import { IconButton } from "@material-ui/core"
 import PropTypes from "prop-types"
 
 const useStyles = makeStyles(theme => ({
-  slider: {
-    display: "flex",
-    alignItems: "center",
-    "& > .slick-list > .slick-track": {
-      paddingTop: 24,
-      paddingBottom: 24,
-    },
-    "& > .slick-list > .slick-track > .slick-slide": {
-      //this could be useful:
-      paddingRight: 200,
-      display: "flex",
-      justifyContent: "center",
-    },
-    // margin: "auto",
+  container: {
+    margin: "auto",
+
     [theme.breakpoints.up("sm")]: {
-      maxWidth: 672, // 336 * 2
+      maxWidth: 672 // 336 * 2
     },
+
     [theme.breakpoints.up("md")]: {
       maxWidth: 1008, // 336 * 3
     },
   },
-  slide: {
-    // maxWidth: 400,
-    transform: "scale(0.8)",
-    transition: "transform 300ms",
-    opacity: 0.5,
-    [theme.breakpoints.down("sm")]: {
-      width: 300,
-    },
+  slider: {
+    display: "flex",
+    alignItems: "center",
 
-    [theme.breakpoints.up("md")]: {
-      width: 400,
+    "& > .slick-list > .slick-track": {
+      // padding: "48px 0",
     },
+  },
+  slide: {
+    transform: "scale(0.8)",
+    outline: "none",
+    transition: "transform 300ms",
+
+    padding: "16px 0",
   },
 
   activeSlide: {
-    transform: "scale(1.1)",
-    opacity: 1,
-    zIndex: 9,
-    position: "relative",
-    filter: "drop-shadow(0px 0px 100px rgba(0, 0, 0, 0.75))",
+    transform: "scale(1)",
+
+    // zIndex: 9,
+    // position: "relative",
   },
 }))
 
 const PrevArrow = ({ onClick }) => {
   return (
-    <IconButton size="small" className="arrow next" onClick={onClick}>
+    <IconButton
+      size="small"
+      onClick={onClick}
+      style={{ marginRight: 16 }}
+    >
       <ArrowLeft style={{ fontSize: 48 }} />
     </IconButton>
   )
@@ -63,7 +58,11 @@ const PrevArrow = ({ onClick }) => {
 
 const NextArrow = ({ onClick }) => {
   return (
-    <IconButton size="small" className="arrow next" onClick={onClick}>
+    <IconButton
+      size="small"
+      onClick={onClick}
+      style={{ marginLeft: 16 }}
+    >
       <ArrowRight style={{ fontSize: 48 }} />
     </IconButton>
   )
@@ -74,6 +73,8 @@ PrevArrow.propTypes = {
 NextArrow.propTypes = {
   onClick: PropTypes.func.isRequired,
 }
+
+
 
 function Carousel() {
   const classes = useStyles()
@@ -88,45 +89,51 @@ function Carousel() {
     )
 
     return (
-      <div key={index}>
-        <div className={className}>
-          <HeroCard
-            hostUsername={lobby.username}
-            hostPicture="https://qph.fs.quoracdn.net/main-qimg-3d69658bf00b1e706b75162a50d19d6c"
-            gameImg="https://cdn.mos.cms.futurecdn.net/MbZ8Yv6WNxjJkPaoQDUPLG-1200-80.jpg"
-            gameName="Minecraft"
-            description={lobby.description}
-            platform="PS5"
-            usesMic={true}
-            sizeStatus="3/5"
-            isContentHidden={index === imageIndex}
-          />
-        </div>
+      <div key={index} className={className} style={{ width: 400 }}>
+        <HeroCard
+          hostUsername={lobby.username}
+          hostPicture="https://qph.fs.quoracdn.net/main-qimg-3d69658bf00b1e706b75162a50d19d6c"
+          gameImg="https://cdn.mos.cms.futurecdn.net/MbZ8Yv6WNxjJkPaoQDUPLG-1200-80.jpg"
+          gameName="Minecraft"
+          description={lobby.description}
+          platform="PS5"
+          usesMic={true}
+          sizeStatus="3/5"
+          isContentHidden={index === imageIndex}
+        />
       </div>
     )
   })
 
   const settings = {
-    infinite: true,
-    lazyLoad: true,
     focusOnSelect: true,
-    // autoplay: true,
-    // autoplaySpeed: 6000,
     speed: 300,
-    pauseOnHover: true,
     slidesToShow: 3,
-    // slidesToScroll: 3,
     centerMode: true,
-    centerPadding: -50,
+
+    className: "center",
+
+    variableWidth: true,
+    adaptiveHeight: true,
+    centerPadding: 0,
+
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
+
+    // pauseOnFocus
+    // pauseOnHover: true,
+    // autoplay: true,
+    // autoplaySpeed: 6000,
+
     beforeChange: (current, next) => setImageIndex(next),
   }
 
   return (
-    <Slider {...settings} className={classes.slider}>
-      {mapCarousel}
-    </Slider>
+    <div className={classes.container}>
+      <Slider {...settings} className={classes.slider}>
+        {mapCarousel}
+      </Slider>
+    </div>
   )
 }
 
@@ -164,3 +171,14 @@ const lobbies = [
     description: "This is my room decription, this should be no more than three lines long...",
   },
 ]
+
+// function Arrow(props) {
+//   let className = props.type === "next" ? "nextArrow" : "prevArrow";
+//   className += " arrow";
+//   const char = props.type === "next" ? "👉" : "👈";
+//   return (
+//     <span className={className} onClick={props.onClick}>
+//       {char}
+//     </span>
+//   );
+// }
