3b618f952fac7afbc0698e35c605e5db38f81a66
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-12-17 21:16:51 -0800

    WIP: Hero Illustrations
    - Added inner scroll

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { CssBaseline } from "@material-ui/core"
 import { TMTheme } from "./atoms"
 import { ThemeProvider } from "@material-ui/core/styles"
 import HomeScreen from "./screens/home-screen/HomeScreen"
+// import LobbyScreen from "./screens/lobby-screen/LobbyScreen"
 
 function App() {
   return (
     <ThemeProvider theme={TMTheme}>
       <CssBaseline />
-      {/* <LobbyScreen /> */}
-      <HomeScreen />
+      <div style={{ backgroundColor: TMTheme.palette.grey[800] }}>
+        {/* <LobbyScreen /> */}
+        <HomeScreen />
+      </div>
     </ThemeProvider>
   )
 }
--- src/screens/home-screen/HomeScreen.js
+++ src/screens/home-screen/HomeScreen.js
@@ -7,14 +7,16 @@ import lobbies from "../../api/dummy-data/lobbies"
 import LobbyGrid from "./molecules/LobbyGrid"
 import AppBar from "../TMAppBar"
 import Carousel from "./molecules/Carousel"
-
 import { AsianScene, WesternScene } from "../../assets/images/"
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    height: "calc(100vh - 64px)",
+    overflowY: "auto",
+  },
   gridContainer: {
     backgroundColor: `${theme.palette.grey[800]}`,
-
-    overflowY: "auto",
+    
     overflowX: "hidden",
     [theme.breakpoints.down("xs")]: {
       display: "block",
@@ -32,6 +34,11 @@ const useStyles = makeStyles(theme => ({
       maxWidth: 1008, // 336 * 3
     },
   },
+  image: {
+    maxHeight: 504,
+    paddingTop: 32,
+    opacity: 0.9,
+  },
 }))
 
 function HomeScreen() {
@@ -40,36 +47,36 @@ function HomeScreen() {
   return (
     <div>
       <AppBar />
-
-      <main style={{  }}>
+      <main>
         <Toolbar />
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          wrap="nowrap"
-          classes={{ root: classes.gridContainer }}
-          spacing={0}
-        >
-          <Hidden xsDown>
-            <Grid item>
-              <img src={AsianScene} style={{ maxHeight: 600, paddingTop: 32, paddingRight: 32, opacity: 0.9 }} />
-            </Grid>
-          </Hidden>
+        <div className={classes.root}>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            wrap="nowrap"
+            classes={{ root: classes.gridContainer }}
+          >
+            <Hidden xsDown>
+              <Grid item>
+                <img src={AsianScene} className={classes.image} style={{ paddingRight: 32 }} />
+              </Grid>
+            </Hidden>
 
-          <Grid item>
-            <div className={classes.contentContainer}>
-              <Carousel />
-              <LobbyGrid lobbies={lobbies} />
-            </div>
+            <Grid item>
+              <div className={classes.contentContainer}>
+                <Carousel />
+                <LobbyGrid lobbies={lobbies} />
+              </div>
+            </Grid>
+
+            <Hidden xsDown>
+              <Grid item>
+                <img src={WesternScene} className={classes.image} style={{ paddingLeft: 32}} />
+              </Grid>
+            </Hidden>
           </Grid>
-
-          <Hidden xsDown>
-            <Grid item>
-              <img src={WesternScene} style={{ maxHeight: 600, paddingTop: 32, paddingRight: 32, opacity: 0.9 }} />
-            </Grid>
-          </Hidden>
-        </Grid>
+        </div>
       </main>
     </div>
   )
