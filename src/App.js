d8f75df19e3d9b7b1c7a46843c40c71f97d75094
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-01-08 22:20:31 -0800

    WIP: Firebase Integration
    - Changed redux store provider from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,66 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { StylesProvider, CssBaseline } from "@material-ui/core"
 import { ThemeProvider } from "@material-ui/core/styles"
-import { Provider } from "react-redux"
 import { useAuthState } from "react-firebase-hooks/auth"
+import { useDispatch, useSelector } from "react-redux"
 import { TMTheme } from "./atoms"
 import HomeScreen from "./screens/home-screen/HomeScreen"
 // import LobbyScreen from "./screens/lobby-screen/LobbyScreen"
-
-import store from "./redux/store"
-import { updateCurrentUser } from "./redux/slices/currentUser/currentUserSlice"
+import {
+  updateCurrentUser,
+  selectUserNeedsAgreement,
+  selectUserNeedsSetup,
+} from "./redux/slices/currentUser/currentUserSlice"
 import { fetchGames } from "./redux/slices/games/gamesSlice"
 import auth from "./api/firebase/auth"
+import AgreementDialog from "./screens/dialogs/agreement/AgreementDialog"
+import CreateProfileDialog from "./screens/home-screen/dialogs/CreateProfileDialog"
 
 function App() {
+  const dispatch = useDispatch()
+
+  // retrive games data from airtable
+  useEffect(() => {
+    dispatch(fetchGames())
+  }, [])
+
   // update current user redux slice
   const [user] = useAuthState(auth)
   useEffect(() => {
     if (user != null) {
-      store.dispatch(updateCurrentUser({ id: user.uid }))
+      // console.log("user changed")
+      dispatch(updateCurrentUser({ id: user.uid }))
     }
   }, [user])
 
-  useEffect(() => {
-    store.dispatch(fetchGames())
-  }, [])
+  const [showAgreement, setShowAgreement] = useState(false)
+  const userNeedsAgreement = useSelector(selectUserNeedsAgreement)
+  if (userNeedsAgreement && !showAgreement) {
+    setShowAgreement(true)
+  }
+
+  const [showCreateProfile, setShowCreateProfile] = useState(false)
+  const userNeedsSetup = useSelector(selectUserNeedsSetup)
+  if (userNeedsSetup && !showCreateProfile && !userNeedsAgreement) {
+    setShowCreateProfile(true)
+  }
 
   return (
-    <Provider store={store}>
-      <StylesProvider injectFirst>
-        <ThemeProvider theme={TMTheme}>
-          <CssBaseline />
-          <div style={{ backgroundColor: TMTheme.palette.grey[800] }}>
-            {/* <LobbyScreen /> */}
-            <HomeScreen />
-          </div>
-        </ThemeProvider>
-      </StylesProvider>
-    </Provider>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={TMTheme}>
+        <CssBaseline />
+        <div style={{ backgroundColor: TMTheme.palette.grey[800] }}>
+          {/* <LobbyScreen /> */}
+          <HomeScreen />
+
+          <AgreementDialog open={showAgreement} onClose={() => setShowAgreement(false)} />
+          <CreateProfileDialog
+            open={showCreateProfile}
+            onClose={() => setShowCreateProfile(false)}
+          />
+        </div>
+      </ThemeProvider>
+    </StylesProvider>
   )
 }
 
